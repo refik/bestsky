9ada4a7ffcb024a5381649fc3b4b602f8f9762b8
Refik Turkeli <refik.rfk@gmail.com>
2010-10-30 10:24:19 +0000

    ranking system based on picture is implemented

--- map/views.py
+++ map/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-import process
+from map.models import Suggestion
+import process, time
 
 def mainPage(request):
   return render_to_response('map/main_page.html')
@@ -9,6 +10,7 @@ def mainPage(request):
 def mapData(request):
   top = request.POST['top']
   bottom = request.POST['bottom']
+  post_id = request.POST['id']
   tp = top.split(',')
   bp = bottom.split(',')
   top_lat = float(tp[0][1:])
@@ -17,9 +19,14 @@ def mapData(request):
   bot_lon = float(bp[1][1:-1])
   lat_dif = top_lat - bot_lat
   lon_dif = top_lon - bot_lon
-  print process.calculate((top_lat,top_lon),lat_dif,lon_dif)
+  answer = process.calculate((top_lat,top_lon),lat_dif,lon_dif)
+  new = Suggestion(unique = int(post_id), coordinates = str(answer))
+  new.save()
+
   return HttpResponse('success')
 
 def getSuggest(request, request_id):
-  answer = request_id * 2
-  return HttpResponse(answer)
+  answer = Suggestion.objects.get(unique = int(request_id))
+  copy = answer
+  answer.delete()
+  return HttpResponse(copy)
--- settings.py
+++ settings.py
@@ -94,7 +94,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'bestsky.map',
